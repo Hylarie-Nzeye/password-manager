29ae5e62b4fe080bb991cfb4a853761c4089057b
Hylarie_Nzeye <nzeyehylarie03@gmail.com>
2025-03-11 10:42:35 +0100

    end backend,frontend

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, send_file, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+import csv
+import time
+timestr= time.strftime("%Y%m%d-%H%M%S")
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SECRET_KEY'] ='secret-key'
@@ -35,14 +39,52 @@ def add_details():
 
 
 
-@app.route('/update')
-def  update_details():
-    return "Hello World"
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def  update_details(id):
+    update_details = PasswordManager.query.get_or_404(id)
+    if request.method == 'POST':
+        update_details.email = request.form['email']
+        update_details.site_url = request.form['site_url']
+        update_details.password = request.form['password']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was a problem updating  Password "
+    else:
+        return render_template('update.html', update_details=update_details)
 
 
-@app.route('/delete')
-def  delete_details():
-    return "Hello World"
+
+@app.route('/delete/<int:id>')
+def  delete_details(id):
+    new_details_to_delete = PasswordManager.query.get_or_404(id)
+    try:
+        db.session.delete(new_details_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "There was a problem deleting that task"
+
+@app.route('/export')
+def export_data():
+    # Crée un fichier CSV
+    with open('dump.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        
+        # Écrire l'en-tête
+        writer.writerow(['id', 'email', 'site_url', 'password'])
+        
+        # Écrire chaque ligne du modèle PasswordManager
+        for item in PasswordManager.query.all():
+            writer.writerow([item.id, item.email, item.site_url, item.password])
+    
+    # Renvoi du fichier CSV comme téléchargement
+    return send_file('dump.csv',
+                     mimetype='text/csv',
+                     download_name=f"Export_password_{timestr}.csv",
+                     as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)     # run our app in debug mode
